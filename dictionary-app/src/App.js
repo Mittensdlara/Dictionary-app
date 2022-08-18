1b99d457879e1771f146c315bb29e8de51bf51cb
Mittensdlara <pooraghaeedelara@gmail.com>
2022-08-18 20:43:59 +0430

    Creating react app

---
+++ dictionary-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo-shecodes.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <img src={logo} alt='logo' width="300px" className='rounded mx-auto d-block'/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
