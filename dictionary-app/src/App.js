f0831bd971c55fdb801a0fd43b21245096d911ef
Mittensdlara <pooraghaeedelara@gmail.com>
2022-08-18 21:14:18 +0430

    update

--- dictionary-app/src/App.js
+++ dictionary-app/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import logo from './logo-shecodes.png'
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
+       <img src={logo} alt='logo' width="300px" className='rounded mx-auto d-block'/>
       </header>
     </div>
   );
