3c4fe867f925071c3e4d62f63dec5420a28eb95c
Mittensdlara <pooraghaeedelara@gmail.com>
2022-08-18 23:21:20 +0430

    Updated

--- dictionary-app/src/App.js
+++ dictionary-app/src/App.js
@@ -6,10 +6,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
+    
         
        <img src={logo} alt='logo' width="300px" className='rounded mx-auto d-block'/>
-      </header>
+   
     </div>
   );
 }
